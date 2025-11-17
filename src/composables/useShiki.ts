dc397117764035c782770c14a4017b3c3d8201b4
Barbapapazes <e.soubiran25@gmail.com>
2025-11-17 11:45:24 +0100

    refactor: simplify shiki

--- src/composables/useShiki.ts
+++ src/composables/useShiki.ts
@@ -1,38 +1,24 @@
-import type { HighlighterCore } from 'shiki/core'
 import type { Language } from '@/types/language'
-import vue from '@shikijs/langs/vue'
+import type { HighlighterCore } from 'shiki/core'
 import { createHighlighterCore } from 'shiki/core'
 import { createJavaScriptRegexEngine } from 'shiki/engine/javascript'
-import vuePHP from '../tm-grammars/vue-php.json'
 
 let highlighter: HighlighterCore | null = null
 
 export async function useShiki(code: MaybeRefOrGetter<string>, lang: MaybeRefOrGetter<Language>) {
   if (!highlighter) {
-    const _vueLang = vue.at(-1)
-    const vueLang = Object.assign({}, _vueLang)
-
-    delete vueLang.embeddedLangsLazy
-    delete vueLang.embeddedLangs
-
     highlighter = await createHighlighterCore({
       themes: [
         import('@shikijs/themes/github-dark'),
         import('@shikijs/themes/github-light'),
       ],
       langs: [
+        import('@shikijs/langs/vue'),
         import('@shikijs/langs/typescript'),
         import('@shikijs/langs/markdown'),
         import('@shikijs/langs/php'),
         import('@shikijs/langs/json'),
         import('@shikijs/langs/html'),
-        import('@shikijs/langs/html-derivative'),
-        import('@shikijs/langs/markdown-vue'),
-        import('@shikijs/langs/vue-directives'),
-        import('@shikijs/langs/vue-interpolations'),
-        import('@shikijs/langs/vue-sfc-style-variable-injection'),
-        vueLang,
-        vuePHP,
       ],
       engine: createJavaScriptRegexEngine(),
     })
